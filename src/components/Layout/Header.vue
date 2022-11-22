b6c1dd1a85a97a6aa8920812bbe40e3bd9185ff2
ukulelehong <1052940613@qq.com>
2022-11-22 17:33:06 +0800

    修改Header完成组件间通信

--- src/components/Layout/Header.vue
+++ src/components/Layout/Header.vue
@@ -5,25 +5,26 @@
       <ul class="nav navbar-nav pull-xs-right">
         <li class="nav-item">
           <!-- Add "active" class when you're on that page" -->
-          <a class="nav-link active" href="">Home</a>
+          <router-link class="nav-link active" to="/">Home</router-link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="">
+          <router-link class="nav-link" to="/article/article-slug-here">
             <i class="ion-compose"></i>&nbsp;New Article
-          </a>
+          </router-link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="">
+          <router-link class="nav-link" to="/settings">
             <i class="ion-gear-a"></i>&nbsp;Settings
-          </a>
+          </router-link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="">Sign in</a>
+          <router-link class="nav-link" to="/login">Sign in</router-link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="">Sign up</a>
+          <router-link class="nav-link" to="/register">Sign up</router-link>
         </li>
       </ul>
     </div>
   </nav>
 </template>
+
--- src/components/Layout/index.vue
+++ src/components/Layout/index.vue
@@ -8,3 +8,9 @@
     <router-view></router-view>
     <Footer />
 </template>
+
+<style scoped>
+  @import '//code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css';
+  @import '//fonts.googleapis.com/css?family=Titillium+Web:700|Source+Serif+Pro:400,700|Merriweather+Sans:400,700|Source+Sans+Pro:400,300,600,700,300italic,400italic,600italic,700italic';
+  @import '//demo.productionready.io/main.css';
+</style>
